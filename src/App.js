e047aae155bf88cae5b92e59f837b9a985a9e0aa
megha1925 <samantmegha0@gmail.com>
2021-06-01 00:40:18 +0530

    css updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
-
+import "./App.css";
 
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 
 //initlaized database
 firebase.initializeApp({
-//config
+  apiKey: "AIzaSyA6O_CgZ6nZvEKl70aMexzz-t-Vri3Tj30",
+  authDomain: "webchatapp-94205.firebaseapp.com",
+  projectId: "webchatapp-94205",
+  storageBucket: "webchatapp-94205.appspot.com",
+  messagingSenderId: "695162592165",
+  appId: "1:695162592165:web:10d48fe37f60b5a70aa64c",
 });
 
 const auth = firebase.auth();
@@ -21,7 +26,7 @@ function App() {
   const [user] = useAuthState(auth);
 
   return (
-    <div>
+    <div className="App">
       <header>
         <SignOut />
       </header>
@@ -38,7 +43,9 @@ const SignIn = () => {
   };
   return (
     <div>
-      <button onClick={signInWithGoogle}>Sign in with google</button>
+      <button className="Sign-in" onClick={signInWithGoogle}>
+        Sign in with google
+      </button>
     </div>
   );
 };
@@ -53,11 +60,10 @@ function SignOut() {
   );
 }
 
-const ChatRoom = () => {
+function ChatRoom() {
+  const dummy = useRef();
   const messagesRef = firestore.collection("messages");
   const query = messagesRef.orderBy("createdAt").limit(25);
-
-
 
   const [messages] = useCollectionData(query, { idField: "id" });
 
@@ -68,7 +74,6 @@ const ChatRoom = () => {
 
     const { uid, photoURL } = auth.currentUser;
 
-
     await messagesRef.add({
       text: formValue,
       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
@@ -77,37 +82,46 @@ const ChatRoom = () => {
     });
 
     setFormValue("");
+    dummy.current.scrollIntoView({ behavior: "smooth" });
   };
 
   return (
-    <div>
-      {messages &&
-        messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+    <div className="">
+      <main>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+
+        <span ref={dummy}></span>
+      </main>
+
       <form onSubmit={sendMessage}>
         <input
           value={formValue}
           onChange={(e) => setFormValue(e.target.value)}
+          placeholder="say something nice"
         />
+
         <button type="submit" disabled={!formValue}>
-          🕊️
+          <i className="far fa-paper-plane" style={{ padding: "5px" }}></i>
         </button>
       </form>
     </div>
   );
-};
+}
 
-const ChatMessage = (props) => {
+function ChatMessage(props) {
   const { text, uid, photoURL } = props.message;
 
-
-  //const messageClass = uid === auth.currentuser.uid ? "sent" : "recieved";
+  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
 
   return (
     <div>
-      <img src={photoURL} />
-      <p>{text}</p>
+      <div className={`message ${messageClass}`}>
+        <img src={photoURL} />
+        <p>{text}</p>
+      </div>
     </div>
   );
-};
+}
 
 export default App;
